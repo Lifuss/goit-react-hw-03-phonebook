64c9fe5a467b46ad3fd4252807ccd97ad24e0db7
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-09-13 16:53:35 +0300

    task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,21 @@ import { StyledDiv } from './App.styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
-
+  componentDidMount() {
+    const items = JSON.parse(localStorage.getItem('contacts'));
+    if (items?.length) {
+      this.setState({ contacts: items });
+    }
+  }
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts.length !== contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
   // test htmlFor *_*
   nameInputId = crypto.randomUUID();
   phoneInputId = crypto.randomUUID();
